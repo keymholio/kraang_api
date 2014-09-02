8fd5ced3100ef11f3b8326207ca68bbf4916ad5d
Andrew Keym <andrew.keym@gmail.com>
2014-09-01 22:21:10 -0400

    Minor settings change

--- kraang_api/settings.py
+++ kraang_api/settings.py
@@ -2,7 +2,6 @@
 Django settings for kraang_api project.
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
